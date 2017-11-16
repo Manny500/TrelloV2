1fe91bae3735bc4c0478f965e7b9e7e59856e62c
Manny <tenoriom@lafayette.edu>
2017-11-16 16:11:47 -0500

    all can register

--- project3/TrelloV2-Profile-Service/src/main/java/com/revature/profile/TrelloV2ProfileServiceApplication.java
+++ project3/TrelloV2-Profile-Service/src/main/java/com/revature/profile/TrelloV2ProfileServiceApplication.java
@@ -2,7 +2,9 @@ package com.revature.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class TrelloV2ProfileServiceApplication {
 
