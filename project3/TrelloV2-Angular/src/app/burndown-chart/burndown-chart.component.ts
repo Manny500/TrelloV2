3d54013c3a6f9baff54306bf896eadf8e89eb0b9
Manny <tenoriom@lafayette.edu>
2017-11-17 15:27:35 -0500

    basic chart up

--- project3/TrelloV2-Angular/src/app/burndown-chart/burndown-chart.component.ts
+++ project3/TrelloV2-Angular/src/app/burndown-chart/burndown-chart.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
+import { Http } from "@angular/http";
+
+import { BurndownChartService } from './burndown-chart.service';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-burndown-chart',
@@ -6,10 +13,96 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./burndown-chart.component.css']
 })
 export class BurndownChartComponent implements OnInit {
-
-  constructor() { }
-
+  
+  constructor(private chartService: BurndownChartService) { }
+  
   ngOnInit() {
+    
+    this.getChart();
   }
 
+  getChart(): void {
+    this.chartService.getChart().subscribe( result => {
+      // this.CurrentNews = result;
+      //lineChartData
+    });
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  // lineChart
+  public lineChartData: Array<any> = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' }
+  ];
+
+  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+  public lineChartOptions: any = {
+    responsive: true
+  };
+
+  public sort = [
+
+    // function custom_sort(a, b) {
+    //   return new Date(a.chartDate).getTime()
+    //       - new Date(b.chartDate).getTime();
+    // }
+
+    // myData["chart"].sort(custom_sort);
+
+  ];
+
+  public lineChartColors: Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+
+  public lineChartLegend: boolean = true;
+
+  public lineChartType: string = 'line';
+
+  public randomize(): void {
+    let _lineChartData: Array<any> = new Array(this.lineChartData.length);
+    for (let i = 0; i < this.lineChartData.length; i++) {
+      _lineChartData[i] = { data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label };
+      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
+        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
+      }
+    }
+    this.lineChartData = _lineChartData;
+  }
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
 }
