50807cefaee2b43dd9b8828cedbaf1ab3e6d3e09
Brandon <bdf1006@aol.com>
2017-11-19 19:57:24 -0500

    View All Users functional

---
+++ project3/TrelloV2-Permissions-Service/src/main/java/com/revature/permissions/repo/TV2UserRepo.java
@@ -0,0 +1,15 @@
+package com.revature.permissions.repo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.permissions.beans.TV2User;
+
+@Repository(value="TV2UserRepoImpl")
+public interface TV2UserRepo extends CrudRepository<TV2User,Integer>{
+
+	List<TV2User> findAll();
+
+}
