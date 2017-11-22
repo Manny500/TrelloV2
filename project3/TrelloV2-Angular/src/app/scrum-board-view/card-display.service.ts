f4aadfc5aa28bd0eb82f418e6e18c4674cdca992
Clement Lin <x4383lin@gmail.com>
2017-11-22 14:48:41 -0500

    User can view cards

---
+++ project3/TrelloV2-Angular/src/app/scrum-board-view/card-display.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+
+import {Card} from './card-display.interface';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class CardCreateService{
+
+
+
+}
