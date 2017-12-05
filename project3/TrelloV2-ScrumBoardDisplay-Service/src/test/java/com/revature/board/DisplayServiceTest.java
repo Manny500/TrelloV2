22d746f0633efdcaa71019109f28a8c090cdcc09
Clement Lin <x4383lin@gmail.com>
2017-12-05 13:12:29 -0500

    mockito: testSaveCard

--- project3/TrelloV2-ScrumBoardDisplay-Service/src/test/java/com/revature/board/DisplayServiceTest.java
+++ project3/TrelloV2-ScrumBoardDisplay-Service/src/test/java/com/revature/board/DisplayServiceTest.java
@@ -3,7 +3,13 @@ package com.revature.board;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.same;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +17,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.w3c.dom.ls.LSSerializer;
 
 import com.revature.board.beans.Card;
 import com.revature.board.repo.BoardRepo;
@@ -56,7 +63,24 @@ public class DisplayServiceTest {
 		
 		Card returned = service.saveCard(card);
 		
+		verify(cardRepoMock, times(1)).save(card);
+		verifyNoMoreInteractions(cardRepoMock);
+		
 		assertEquals(card, returned);
+	}
+	@Test
+	public void testfindByLId() {
+		//int lId, int cVerify, int cWorth, String cTitle, String cDescription
+		Card card1 = new Card(2,2,2,"Mockito is thirsty", "Please drink Mockito");
+		Card card2 = new Card(2,3,3,"HelloWorld", "JavaWorld");
+		List<Card> ls = new ArrayList<Card>();
+		ls.add(card1);
+		ls.add(card2);
+		when(cardRepoMock.findByLId(2)).thenReturn(ls);
+		
+		List<Card> returned = service.findByLaneId(2);
+		
+		assertEquals(ls, returned);
 	}
 }
 
